ae32a1f48bc11ba8c85c8e259f7d47d553f83c5d
sagar wankhede <swankhede639@gmail.com>
2024-08-22 17:38:55 +0530

    added two div className of first container and second is Current_location

---
+++ src/component/CurrentPosition.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+
+const CurrentPosition = () => {
+    return (
+<div className='container'>
+    <div className="Current_location">
+        <h1>Current Location</h1>
+    </div>
+
+</div>
+    )
+}
+export default CurrentPosition;

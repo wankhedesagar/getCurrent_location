b21d269d22541f5d4c3fbf1b70b5c98c2b087fca
sagar wankhede <swankhede639@gmail.com>
2024-08-22 17:34:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import CurrentPosition from "./component/CurrentPosition";
+
+function App() {
+
+  return (     
+      <div>
+        <CurrentPosition/>
+      </div>
+  );
+}
+
+export default App;
